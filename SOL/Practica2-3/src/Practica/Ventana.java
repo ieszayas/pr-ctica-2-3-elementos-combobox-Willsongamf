08686a0b9b4c413ff298d1456ffa9474c2374ba2
Rodrigo Calvo Mèrida <160269872+Willsongamf@users.noreply.github.com>
2024-10-02 16:15:00 +0200

    Parte_3 Confirmada

--- SOL/Practica2-3/src/Practica/Ventana.java
+++ SOL/Practica2-3/src/Practica/Ventana.java
@@ -18,12 +18,12 @@ public class Ventana extends javax.swing.JFrame {
      * Creates new form Ventana
      */
     public Ventana() {
-        try{
-             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        }catch(Exception e){
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Exception e) {
             System.out.println("Error en el look and feel");
         }
-       
+
         initComponents();
 
     }
@@ -43,6 +43,8 @@ public class Ventana extends javax.swing.JFrame {
         Texto_Añdir = new javax.swing.JLabel();
         Boton_AgregarTodos = new javax.swing.JButton();
         Boton_BorrarTodos = new javax.swing.JButton();
+        Combobox_Selecion = new javax.swing.JComboBox<>();
+        Boton_Papelera = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -75,47 +77,68 @@ public class Ventana extends javax.swing.JFrame {
             }
         });
 
+        Combobox_Selecion.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Primero", "Segundo" }));
+
+        Boton_Papelera.setIcon(new javax.swing.ImageIcon("C:\\Users\\DAM2_09\\Documents\\GitHub\\pr-ctica-2-3-elementos-combobox-Willsongamf\\SOL\\Practica2-3\\src\\Practica\\trash.png")); // NOI18N
+        Boton_Papelera.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Boton_PapeleraActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(Boton_AgregarTodos)
-                .addGap(37, 37, 37)
-                .addComponent(Boton_BorrarTodos)
-                .addGap(28, 28, 28))
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGap(16, 16, 16)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(16, 16, 16)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(Texto_Añdir)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(Campo_Añadir, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(105, 105, 105)
-                                .addComponent(ComboBox_Modulos, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(Boton_Añadir)
+                                .addGap(85, 85, 85)
+                                .addComponent(Boton_AgregarTodos)
+                                .addGap(29, 29, 29)
+                                .addComponent(Boton_BorrarTodos))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(Texto_Añdir)
+                                .addGap(130, 130, 130)))
+                        .addContainerGap(39, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(28, 28, 28)
-                        .addComponent(Boton_Añadir)))
-                .addContainerGap(39, Short.MAX_VALUE))
+                        .addComponent(Campo_Añadir, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Combobox_Selecion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(ComboBox_Modulos, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(Boton_Papelera, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(22, 22, 22))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(38, 38, 38)
                 .addComponent(Texto_Añdir)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Campo_Añadir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(ComboBox_Modulos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(Boton_Añadir)
-                .addGap(1, 1, 1)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Boton_AgregarTodos)
-                    .addComponent(Boton_BorrarTodos))
-                .addContainerGap(141, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(Combobox_Selecion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Boton_Papelera)
+                            .addComponent(ComboBox_Modulos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(Boton_AgregarTodos)
+                            .addComponent(Boton_BorrarTodos)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(69, 69, 69)
+                        .addComponent(Campo_Añadir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(Boton_Añadir)))
+                .addContainerGap(117, Short.MAX_VALUE))
         );
 
         pack();
@@ -143,19 +166,42 @@ public class Ventana extends javax.swing.JFrame {
     }//GEN-LAST:event_Boton_BorrarTodosActionPerformed
 
     private void Boton_AgregarTodosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton_AgregarTodosActionPerformed
-        ArrayList<String> lista_modulos = new ArrayList();
-        lista_modulos.add("Lenguaje de Marcas");
-        lista_modulos.add("Programacion");
-        lista_modulos.add("Base De Datos");
-        lista_modulos.add("Entornos");
-        lista_modulos.add("FOL");
-        lista_modulos.add("Sistemas");
+        ArrayList<String> lista_modulos = crearModulos(Combobox_Selecion.getSelectedItem());
 
         for (String modulo : lista_modulos) {
-            añadirModulo(modulo,true);
+            añadirModulo(modulo, true);
 
         }
     }//GEN-LAST:event_Boton_AgregarTodosActionPerformed
+
+    private ArrayList<String> crearModulos(Object curso) {
+        ArrayList<String> lista_modulos = new ArrayList();
+        if (curso.getClass() != String.class) {
+
+        }
+        String other = (String) curso;
+        if (other.equalsIgnoreCase("Primero")) {
+            lista_modulos.add("Lenguaje de Marcas");
+            lista_modulos.add("Programacion");
+            lista_modulos.add("Base De Datos");
+            lista_modulos.add("Entornos");
+            lista_modulos.add("FOL");
+            lista_modulos.add("Sistemas");
+        } else {
+            lista_modulos.add("Acceso a Datos");
+            lista_modulos.add("ProgramacionMovil");
+            lista_modulos.add("ProgramacionProcesos");
+            lista_modulos.add("Interfaces");
+            lista_modulos.add("Empresa");
+            lista_modulos.add("Sistemas");
+            lista_modulos.add("Ingles");
+        }
+        return lista_modulos;
+    }
+    private void Boton_PapeleraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton_PapeleraActionPerformed
+
+        ComboBox_Modulos.removeItem(ComboBox_Modulos.getSelectedItem());
+    }//GEN-LAST:event_Boton_PapeleraActionPerformed
 
     private boolean controlCampoAñadir() {
         if (Campo_Añadir.getText().isEmpty()) {
@@ -228,8 +274,10 @@ public class Ventana extends javax.swing.JFrame {
     private javax.swing.JButton Boton_AgregarTodos;
     private javax.swing.JButton Boton_Añadir;
     private javax.swing.JButton Boton_BorrarTodos;
+    private javax.swing.JButton Boton_Papelera;
     private javax.swing.JTextField Campo_Añadir;
     private javax.swing.JComboBox<String> ComboBox_Modulos;
+    private javax.swing.JComboBox<String> Combobox_Selecion;
     private javax.swing.JLabel Texto_Añdir;
     // End of variables declaration//GEN-END:variables
 }
